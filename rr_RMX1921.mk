0b223640292747141f359ece32c07d947c291f6c
Sagarrokade006 <sagarrokade006@gmail.com>
2020-08-20 10:55:30 +0000

    Update qti-telephony-common from DRG

--- rr_RMX1921.mk
+++ rr_RMX1921.mk
@@ -15,7 +15,7 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 PRODUCT_BRAND := Realme
 PRODUCT_DEVICE := RMX1921
 PRODUCT_MANUFACTURER := Realme
-PRODUCT_NAME := aosp_RMX1921
+PRODUCT_NAME := rr_RMX1921
 PRODUCT_MODEL := Realme XT
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
